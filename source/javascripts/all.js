da2b20753ac68f9ba3188c6442e3b985e1300609
Nuno Coelho Santos <nuno@nunocoelhosantos.com>
2016-05-12 10:28:51 +0900

    This should improve JS performance a little bit

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,15 +1,19 @@
 $(document).ready(function() {
 
   var iphoneContainer = $(".app-overview")
+  var calloutsVisible = false
+  var scrollPosition = 0
 
   $(window).scroll(function() {
-    var scrollPosition = $(window).scrollTop();
-    if (scrollPosition >= 200) {
+    scrollPosition = $(window).scrollTop();
+    if (scrollPosition >= 200 && !calloutsVisible) {
       iphoneContainer.addClass("callouts-visible");
       iphoneContainer.removeClass("callouts-hidden");
-    } else {
+      calloutsVisible = true
+    } else if (scrollPosition < 200 && calloutsVisible){
       iphoneContainer.removeClass("callouts-visible");
       iphoneContainer.addClass("callouts-hidden");
+      calloutsVisible = false
     }
   });
 
